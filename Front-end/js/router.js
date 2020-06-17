3992f2a71243fdd0590b26946335a2249c21c9bf
Raphael <6105939@cstj.qc.ca>
2020-06-17 13:29:25 -0400

    modif produit version mobile + working carrousel

--- Front-end/js/router.js
+++ Front-end/js/router.js
@@ -1,11 +1,13 @@
 import accueil from './components/horsConnexion/accueil'
 import connexion from './components/horsConnexion/connexion'
 import inscription from './components/horsConnexion/inscription'
+import modificationProduit from './components/admin/modificationProduit'
 
 export default new VueRouter({
     routes: [
         { path: '/', component: accueil },
         { path:'/connexion', component: connexion},
         { path:'/inscription', component: inscription},
+        { path:'/modificationProduit', component: modificationProduit }
     ]
 })
