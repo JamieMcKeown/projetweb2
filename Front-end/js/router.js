ff045bd85ce141bbe5ff71106f6bf69e49c1bb6b
Raphael <6105939@cstj.qc.ca>
2020-06-18 20:59:03 -0400

    Modif Profil version web terminé à 100%.

--- Front-end/js/router.js
+++ Front-end/js/router.js
@@ -2,12 +2,14 @@ import accueil from './components/horsConnexion/accueil'
 import connexion from './components/horsConnexion/connexion'
 import inscription from './components/horsConnexion/inscription'
 import modificationProduit from './components/admin/modificationProduit'
+import modificationProfil from './components/admin/modificationProfil'
 
 export default new VueRouter({
     routes: [
         { path: '/', component: accueil },
         { path:'/connexion', component: connexion},
         { path:'/inscription', component: inscription},
-        { path:'/modificationProduit', component: modificationProduit }
+        { path:'/modificationProduit', component: modificationProduit },
+        { path:'/modificationProfil', component: modificationProfil },
     ]
 })
