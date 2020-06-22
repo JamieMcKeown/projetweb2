2ae37606f73e26fa0a5f570fea64d1608fc08301
MPoliquin <matpoliquin@gmail.com>
2020-06-22 05:23:22 -0400

    adding a fetch user function to accueil

--- Front-end/js/components/horsConnexion/accueil.js
+++ Front-end/js/components/horsConnexion/accueil.js
@@ -1,13 +1,18 @@
 import tpl from '../../utils/avecTemplateHtml'
+import { http_get } from '../../utils/request'
 
 export default tpl({
     template: './html/horsConnexion/accueil.html',
     data () {
         return {
+            users: [],
+            potagers: [],
+            api: "http://api.test/api/"
         }
     },
     mounted(){
-       
+       this.fetchRandomUser()
+       console.log(this.users)
     },
     methods: {
         homepageRoute() {
@@ -19,6 +24,30 @@ export default tpl({
         },
         inscriptionPage() {
             this.$router.push("/inscription")
+        },
+
+        fetchRandomUser() {
+            let api_url = this.api + "user/random/4"
+            http_get(api_url).then(data => {
+                //console.log(data)
+                for (let i = 0; i < 4; i++){
+                    let id = data[i].id
+                    let prenom = data[i].prenom
+                    let nom = data[i].nom
+                    let toAdd = {
+                        userID: id,
+                        firstname: prenom,
+                        lastname: nom,
+                    }
+                    //console.log(toAdd)
+                    this.users = this.users.concat(toAdd)
+                    console.log(this.users)
+                }
+            })
+        },
+
+        fetchRandomPotager() {
+            let api_url = this.api + "potager/random/4"
         }
     },
 })
