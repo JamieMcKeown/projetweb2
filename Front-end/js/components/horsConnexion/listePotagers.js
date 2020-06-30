5c6652ddeeabece6d6a45d8a5aa53c4404cdd169
MPoliquin <matpoliquin@gmail.com>
2020-06-30 10:11:54 -0400

    listePotagers done

--- Front-end/js/components/horsConnexion/listePotagers.js
+++ Front-end/js/components/horsConnexion/listePotagers.js
@@ -1,11 +1,14 @@
 import tpl from '../../utils/avecTemplateHtml'
+import { http_get } from '../../utils/request'
 
 // export du object literal complet représentant le component
 export default tpl({
     template: './html/horsConnexion/listePotagers.html',
     data(){
         return {
+            potagers: [],
             isActive: true,
+            api: "http://api.test/api/potager"
         }
     }, //end of data
     methods:  {
@@ -32,7 +35,20 @@ export default tpl({
                 this.isActive = false
             }
         },
+
+        fetchAllPotager(){
+            let url = this.api
+
+            http_get(url).then(data => {
+                this.potagers = data
+                console.log(this.potagers)
+            })
+        },
       
 
     },// end of mounted
+
+    mounted() {
+        this.fetchAllPotager()
+    }
 })
