d84accf47d7170e6dd63fc13680169632e989236
MPoliquin <matpoliquin@gmail.com>
2020-07-02 10:24:49 -0400

    added function for incoming offers exchange

--- Front-end/js/components/admin/pageOffres.js
+++ Front-end/js/components/admin/pageOffres.js
@@ -1,15 +1,24 @@
 import tpl from '../../utils/avecTemplateHtml'
+import { http_get } from '../../utils/request'
 
 // export du object literal complet représentant le component
 export default tpl({
     template: './html/admin/pageOffres.html',
     data() {
         return {
-            
+            api: "http://api.test/api/echange/",
+            id: "",
+            prenom: "",
+            nom: "",
+            incomingOffers: [],
+
+
         }
     }, //end of data
     mounted(){
         this.preventDisconnectedUser()
+        this.getInfos()
+        this.getIncomingOffers(this.id)
     },//end of mounted
     methods: {
         homepageRoute() {
@@ -33,5 +42,22 @@ export default tpl({
                 this.$router.push("/")
             }
         },
+
+        getInfos() {
+            let data = localStorage.getItem('data')
+            let parsed = JSON.parse(data)
+            this.id = parsed.id
+            this.prenom = parsed.prenom
+            this.nom = parsed.nom
+        },
+
+        getIncomingOffers(id) {
+            let url = this.api + "to/" + id
+
+            http_get(url).then(data => {
+                this.incomingOffers = data
+                console.log(data)
+            })
+        }
     }//end of methods
 })
