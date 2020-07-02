9b4487ee7c1ab538eccd871cdc8c1a1a697d1ca4
MPoliquin <matpoliquin@gmail.com>
2020-07-02 06:03:18 -0400

    produit dynamiser

--- Front-end/js/components/horsConnexion/produit.js
+++ Front-end/js/components/horsConnexion/produit.js
@@ -1,4 +1,5 @@
 import tpl from '../../utils/avecTemplateHtml'
+import { http_get } from '../../utils/request'
 
 // export du object literal complet représentant le component
 export default tpl({
@@ -8,10 +9,21 @@ export default tpl({
             isActive: true,
             connected: false,
             user: "",
+            recolteID: this.$route.params.id,
+            api: "http://api.test/api/recolte/",
+            jardinierID: "",
+            jardinierPrenom: "",
+            jardinierNom: "",
+            potagerID: "",
+            produceDescription: "",
+            produceQte: "",
+            produceID: "",
+            produceName: "",
         }
     },// end data
     mounted() {
         this.checkIfUserIsConnected()
+        this.fetchProduce(this.recolteID)
     },
     methods: {
         homepageRoute() {
@@ -60,6 +72,30 @@ export default tpl({
     
                 console.log(this.user)
             }
+        },
+
+        fetchProduce(id) {
+            let url = this.api + id
+
+            http_get(url).then(data => {
+                console.log(data)
+                this.jardinierID = data[0].potager_owner_id
+                this.jardinierPrenom = data[0].potager_owner_prenom
+                this.jardinierNom = data[0].potager_owner_nom
+                this.potagerID = data[0].potager_id
+                this.produceDescription = data[0].description
+                this.produceQte = data[0].quantite
+                this.produceID = data[0].recolte_id
+                this.produceName = data[0].type
+            })
+        },
+
+        goToPotager(id){
+            this.$router.push("/detailsPotager/" + id)
+        },
+
+        goToJardinier(id){
+            this.$router.push("/detailsJardinier/" + id)
         },
     }
 })
